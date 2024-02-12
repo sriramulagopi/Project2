00d4a9f8f4ffe0917f1fcba68f9c3f1206965fd6
sriramulagopi <srgopi1234@gmail.com>
2024-02-12 20:00:08 +0530

    Project1

---
+++ script.js
@@ -0,0 +1,335 @@
+let quizData = [
+    {
+        "category": "General Knowledge",
+        "questions": [
+            {
+                "question": "What is the currency of Japan?",
+                "options": ["Yen", "Euro", "Dollar", " Rupee"],
+                "answer": "Yen"
+            },
+            {
+                "question": "What is the capital of Australia?",
+                "options": ["Sydney", "GMelbourne", "Canberra", "Perth"],
+                "answer": "Canberra"
+            },
+            {
+                "question": "Who painted the Mona Lisa?",
+                "options": ["Vincent van Gogh", "Pablo Picasso", "Leonardo da Vinci", "Michelangelo"],
+                "answer": "Leonardo da Vinci"
+            },
+            {
+                "question": "What is the largest ocean in the world?",
+                "options": ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean"],
+                "answer": "Pacific Ocean"
+            },
+            {
+                "question": "Which planet is known as the Red Plane?",
+                "options": ["Venus", "Mars", "Jupiter", "Saturn"],
+                "answer": "Mars"
+            },
+
+            // more questions...
+        ]
+    },
+    {
+        "category": "Economy",
+        "questions": [
+            {
+                "question": "When is Earth Day observed?",
+                "options": ["20 March", "22 April", "5 June", "24 September"],
+                "answer": "22 April"
+            },
+            {
+                "question": " When was the first Earth Day observed?",
+                "options": ["1992", "2001", "1970", "1982"],
+                "answer": "1970"
+            },
+            {
+                "question": "In which year NSE was established?",
+                "options": ["1978", "1985", "1994", "2000"],
+                "answer": "1994"
+            },
+            {
+                "question": "When a person has a saving account in the bank , the bank assumes the position of ___?",
+                "options": ["Debtor", "Creditor", "Agency", "Depositor"],
+                "answer": "Debtor"
+            },
+            {
+                "question": "When is Earth Day observed?",
+                "options": ["20 March", "22 April", "5 June", "24 September"],
+                "answer": "22 April"
+            }
+            // more questions...
+        ]
+    },
+    {
+        "category": "Resoning",
+        "questions": [
+            {
+                "question": "Today it is Thursday.After 132 days,it will be",
+                "options": ["Monday", "Sunday", "Wednesday", "Thursday"],
+                "answer": "Wednesday"
+            },
+            {
+                "question": "Look at this series: 12, 11, 13, 12, 14, 13, … What number should come next?",
+                "options": ["10", "16", "13", "15"],
+                "answer": "15"
+            },
+            {
+                "question": " Which word is the odd man out?",
+                "options": ["fondness", "liking", "attachment", "hate"],
+                "answer": "hate"
+            },
+            {
+                "question": " CUP : LIP :: BIRD : ?",
+                "options": ["FOREST", "BEAK", "BUSH", "GRASS"],
+                "answer": "BEAK"
+            },
+            {
+                "question": " Melt : Liquid :: Freeze :?",
+                "options": ["Solid", "Condense", "Push", "Ice"],
+                "answer": "Solid"
+            },
+            // more questions...
+        ]
+    },
+    {
+        "category": "Aptitude",
+        "questions": [
+            {
+                "question": "What is the average of first five multiples of 12?",
+                "options": ["36", "38", "40", "42"],
+                "answer": "36"
+            },
+            {
+                "question": "What percentage of numbers from 1 to 70 have 1 or 9 in the unit's digit?",
+                "options": ["1", "14", "20", "21"],
+                "answer": "20"
+            },
+            {
+                "question": "A is two years older than B who is twice as old as C. If the total of the ages of A, B and C be 27, then how old is B?",
+                "options": ["7", "8", "9", "10"],
+                "answer": "10"
+            },
+            {
+                "question": "A can do a work in 15 days and B in 20 days. If they work on it together for 4 days, then the fraction of the work that is left is",
+                "options": ["1/4", "1/10", "7/15", "8/15"],
+                "answer": "8/15"
+            },
+            {
+                "question": "The cost price of 20 articles is the same as the selling price of x articles. If the profit is 25%, then the value of x is:",
+                "options": ["15", "16", "18", "25"],
+                "answer": "16"
+            },
+            // more questions...
+        ]
+    },
+    {
+        "category": "Sports",
+        "questions": [
+            {
+                "question": "How many chapters are there in the Olympic Charter?",
+                "options": ["5", "6", "7", "8"],
+                "answer": "5"
+            },
+            {
+                "question": "Which cricketer has the record of highest individual score in ODIs?",
+                "options": ["Rohit Sharma", "Martin Guptill", "Chris Gayle", "Virender Sehwag"],
+                "answer": "Rohit Sharma"
+            },
+            {
+                "question": "Where was the first Winter Olympics held?",
+                "options": ["Greece", "France", "Germany", "England"],
+                "answer": "France"
+            },
+            {
+                "question": "Which country has won maximum number of titles in Uber Cup?",
+                "options": ["Japan", "US", "Indonesia", "China"],
+                "answer": "China"
+            },
+            {
+                "question": "What is the standard width of each wicket in cricket?",
+                "options": ["9 inches", "10 inches", "1 inches", "12 inches"],
+                "answer": "9 inches"
+            },
+            // more questions...
+        ]
+    },
+    {
+        "category": "Chemistry",
+        "questions": [
+            {
+                "question": "Which among the following is known as Quick Lime?",
+                "options": ["CaO", "CaCO2", "Ca(OH)2", "CaCl2"],
+                "answer": "CaO"
+            },
+            {
+                "question": "PVC is a polymer of__?",
+                "options": ["Propane", "Vinyl chloride", "Styrene", "Carbonates"],
+                "answer": "Vinyl chloride"
+            },
+            {
+                "question": "Water Gas is a mixture of?",
+                "options": ["CO and H2", "CO and H2O", "CO2 and H2", " None of the Above"],
+                "answer": "CO2 and H2"
+            },
+            {
+                "question": "Which of these is the only metal that is liquid at ordinary room temperature and pressure?",
+                "options": ["Copper", "Zinc", "Mercury", "Beryllium"],
+                "answer": "Mercury"
+            },
+            {
+                "question": "Which among the following is known as Quick Lime?",
+                "options": ["Platinum", "Cadmium", "Cobalt", "Silver"],
+                "answer": "Cadmium"
+            },
+            // more questions...
+        ]
+    },
+    {
+        "category": "Biology",
+        "questions": [
+            {
+                "question": "Plants receive their nutrients mainly from",
+                "options": ["chlorophyll", "atmosphere", "light", "soil"],
+                "answer": "soil"
+            },
+            {
+                "question": "Plants synthesis protein from",
+                "options": ["starch", "sugar", "amino acids", "fatty acids"],
+                "answer": "amino acids"
+            },
+            {
+                "question": "Plants absorb dissolved nitrates from soil and convert them into",
+                "options": ["free nitrogen", "urea", "ammonia", " proteins"],
+                "answer": "free nitrogen"
+            },
+            {
+                "question": "Process of cell division can take place by",
+                "options": ["heterosis", "fusion", "mitosis", "None of these"],
+                "answer": "mitosis"
+            },
+            {
+                "question": "Most highly intelligent mammals are",
+                "options": ["whales", "dolphins", "elephants", "kangaroos"],
+                "answer": "dolphins"
+            },
+            // more questions...
+        ]
+    },
+    // more category...
+];
+let topics = [];
+let list = [];
+const selectTopic = document.querySelectorAll(".topics>div");
+const div3 = document.querySelector(".div3");
+const quiz = document.querySelector(".quiz>button")
+const section1 = document.querySelector(".section1");
+const Name = document.querySelector(".name");
+function display(){
+    if(div3.style.display==="none"){
+        div3.style.display="flex";
+    }
+    else{
+        div3.style.display="none";
+    }
+}
+function addAndremove(topic,i){
+    let check = getComputedStyle(topic.children[1]).display;
+    if(check==="none"){
+        topic.children[1].style.display="flex";
+        topic.children[0].style.background="#FCC822"
+        topics.push(topic.children[0].innerText)
+        
+    }
+    else{
+        topic.children[1].style.display="none";
+        topic.children[0].style.background="#828282"
+        topics.splice(i,1);
+    }
+}
+for (let i=0;i<selectTopic.length;i++){
+    selectTopic[i].addEventListener("click",()=>{
+        addAndremove(selectTopic[i],i);
+    });
+}
+const form1 = document.querySelector("form");
+quiz.addEventListener("click",()=>{
+    Name.innerText=prompt("Enter Your Name: ")
+    Name.style.border=" 4px solid green";
+    Name.style.color="green";
+    if (topics.length>0){
+        section1.style.display="none";
+        display();
+        startQuiz(topics);
+    }
+    form1.style.display="flex";
+})
+function startQuiz(topics){
+    quizData.forEach((items)=>{
+        if(topics.includes(items.category)){
+            list.push(...items.questions);
+        }
+    })
+    open(list);
+}
+let index = 0;
+let answers = [];
+let finalAnwers=[];
+function open(questions){
+    const question1 = document.querySelector(".question");
+    const options1 = document.querySelectorAll(".options label");
+    const options2 = document.querySelectorAll(".options input");
+    finalAnwers.push(questions[index].answer);
+    question1.innerText=questions[index].question;
+    for (let i=0;i<4;i++){
+        options1[i].innerText=questions[index].options[i];
+        options2[i].value=questions[index].options[i];
+    }
+    if(answers[index]){
+        form1.option.value = answers[index];
+    }
+}
+const impbtn = document.querySelector("#impBtn");
+const impbtn1 = document.querySelector("#impBtn1");
+function previous(){
+    if(index===0){
+        alert("This is First Question");
+    }
+    else{
+        index--;
+        open(list);
+    }
+}
+function next(){
+    let answer = form1.option.value;
+    if (answer){
+        answers[index]=answer;
+    }
+    else{
+        answers[index]=undefined;
+    }
+    if(index===list.length-1){
+        let score = 0;
+        for (let i=0;i<finalAnwers.length;i++){
+            if(answers[i]===finalAnwers[i]){
+                score+=5;
+            }
+        }
+        impbtn1.children[1].addEventListener("click",()=>{
+            impbtn1.children[1].innerText="Submit";
+            impbtn1.style.display="none"
+            impbtn.style.display="flex"
+            impbtn.children[0].innerText=`Your Score is ${score} out of ${finalAnwers.length*5}.`
+        })
+
+    }
+    else{
+        index++;
+        form1.reset();
+        open(list);
+    }
+}
+impbtn.children[1].addEventListener("submit",()=>{
+
+})
